8da1938085dc3d94d4c131b9da2d5a542ac61a43
BryRoi <108722108+BrydelRoi@users.noreply.github.com>
2022-09-13 10:49:31 +0800

    check in prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,8 @@ export const environment = {
   production: true,
 
   masterfileAPI: 'https://masterfile-development.azurewebsites.net/api/v1',
-  authAPI: 'https://identity-development.azurewebsites.net/api/v1'
+  authAPI: 'https://identity-development.azurewebsites.net/api/v1',
+  addressInformationAPI: 'https://psgcaddress-development.azurewebsites.net/api/v1',
+  mediaAPI: 'http://13.75.89.190:11198/api/v1',
+  appId:'EHR-QA'
 };
