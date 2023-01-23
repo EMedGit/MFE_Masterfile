ff4d5c7291a8ca234583f8be93414a1b9bf341a3
Kenneth patzb0y <kpatangan@gmail.com>
2023-01-23 14:55:03 +0800

    commit prod environment()

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,24 @@
 export const environment = {
   production: true,
 
-  masterfileAPI: 'https://masterfile-development.azurewebsites.net/api/v1',
-  authAPI: 'https://identity-development.azurewebsites.net/api/v1',
-  addressInformationAPI: 'https://psgcaddress-development.azurewebsites.net/api/v1',
-  mediaAPI: 'http://13.75.89.190:11198/api/v1',
-  appId:'EHR-QA'
+  // QA
+  // masterfileAPI: 'https://masterfile-development.azurewebsites.net/api/v1',
+  // authAPI: 'https://identity-development.azurewebsites.net/api/v1',
+  // addressInformationAPI: 'https://psgcaddress-development.azurewebsites.net/api/v1',
+  // mediaAPI: 'http://13.75.89.190:11198/api/v1',
+  // appId:'EHR-QA'
+
+  //MABINI
+  // masterfileAPI: 'https://masterfile-live-api.azurewebsites.net/api/v1',
+  // authAPI: 'https://identity-live-api.azurewebsites.net/api/v1',
+  // addressInformationAPI: 'https://psgcaddress-live-api.azurewebsites.net/api/v1',
+  // mediaAPI: 'http://13.75.89.190:11195/api/v1',
+  // appId:'EHR-MABINI'
+
+   //LAUNION
+  masterfileAPI: 'https://launion-masterfile-live-api.azurewebsites.net/api/v1',
+  authAPI: 'https://launion-identity-live-api.azurewebsites.net/api/v1',
+  addressInformationAPI: 'https://psgcaddress-live-api.azurewebsites.net/api/v1',
+  mediaAPI: 'http://13.75.89.190:11195/api/v1',
+  appId:'EHR-LAUNION'
 };
