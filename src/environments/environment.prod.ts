ee9b7ebd7f7937a6fef5f9df809320a2a7c9bd1c
Kenneth patzb0y <kpatangan@gmail.com>
2023-06-13 11:31:13 +0800

    commit pasig settings()

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
 
   // QA
-  masterfileAPI: 'https://masterfile-development.azurewebsites.net/api/v1',
-  authAPI: 'https://identity-development.azurewebsites.net/api/v1',
-  addressInformationAPI: 'https://psgcaddress-development.azurewebsites.net/api/v1',
-  mediaAPI: 'http://13.75.89.190:11198/api/v1',
-  appId:'EHR-QA'
+  // masterfileAPI: 'https://masterfile-development.azurewebsites.net/api/v1',
+  // authAPI: 'https://identity-development.azurewebsites.net/api/v1',
+  // addressInformationAPI: 'https://psgcaddress-development.azurewebsites.net/api/v1',
+  // mediaAPI: 'http://13.75.89.190:11198/api/v1',
+  // appId:'EHR-QA'
 
   //MABINI
   // masterfileAPI: 'https://masterfile-live-api.azurewebsites.net/api/v1',
@@ -71,10 +71,17 @@ export const environment = {
     //  mediaAPI: 'http://13.75.89.190:11195/api/v1',
     //  appId:'EHR-ZAMBALES'
 
-    //LABXASIA
-     masterfileAPI: 'https://labxasia-masterfile-live-api.azurewebsites.net/api/v1',
-     authAPI: 'https://labxasia-identity-live-api.azurewebsites.net/api/v1',
-     addressInformationAPI: 'https://psgcaddress-live-api.azurewebsites.net/api/v1',
-     mediaAPI: 'http://13.75.89.190:11195/api/v1',
-     appId:'EHR-LABXASIA'
+    //pasig
+    //  masterfileAPI: 'https://pasig-masterfile-live-api.azurewebsites.net/api/v1',
+    //  authAPI: 'https://pasig-identity-live-api.azurewebsites.net/api/v1',
+    //  addressInformationAPI: 'https://psgcaddress-live-api.azurewebsites.net/api/v1',
+    //  mediaAPI: 'http://13.75.89.190:11195/api/v1',
+    //  appId:'EHR-pasig'
+
+       //PASIG
+       masterfileAPI: 'https://pasig-masterfile-live-api.azurewebsites.net/api/v1',
+       authAPI: 'https://pasig-identity-live-api.azurewebsites.net/api/v1',
+       addressInformationAPI: 'https://psgcaddress-live-api.azurewebsites.net/api/v1',
+       mediaAPI: 'http://13.75.89.190:11195/api/v1',
+       appId:'EHR-PASIG'
 };
